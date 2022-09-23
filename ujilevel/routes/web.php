447f1890dcbb9d801af1191704924736abedbad5
faraz3 <farazyanuar1@gmail.com>
2022-09-23 10:26:03 +0700

    login + adminLTE

--- ujilevel/routes/web.php
+++ ujilevel/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +18,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/login', [AuthController::class, 'index'])->name('login');
+Route::post('/proses_login', [AuthController::class, 'proses_login'])->name('proses_login');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::group(['middleware' => ['cek_login:admin']], function () {
+        Route::get('admin', [AdminController::class, 'index'])->name('admin');
+    });
+    Route::group(['middleware' => ['cek_login:user']], function () {
+        Route::get('user', [UserController::class, 'index'])->name('user');
+    });
 });
